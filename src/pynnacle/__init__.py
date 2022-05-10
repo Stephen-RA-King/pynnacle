129a774ab8583b1336eb2952099f39def9647750
Stephen-RA-King <33905365+Stephen-RA-King@users.noreply.github.com>
2022-05-10 13:24:32 +0100

    Restructure package

--- src/pynnacle/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Top-level package for pynnacle."""
-
-__title__ = "pynnacle"
-__author__ = "Stephen R A King"
-__description__ = "boilerplate you need to create a Python package."
-__email__ = "name@isp.com"
-__version__ = "0.1.0"
-__license__ = "MIT"
-__copyright__ = "Copyright 2022 Stephen R A King"
